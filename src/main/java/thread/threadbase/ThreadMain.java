0303e18bf6859c7bcb53d723fda1e6ed366ed507
Jackchang <1114950132@qq.com>
2021-04-09 06:43:17 +0800

    thread:interrupt

---
+++ src/main/java/thread/threadbase/ThreadMain.java
@@ -0,0 +1,27 @@
+package thread.threadbase;
+
+public class ThreadMain {
+//    public static void main(String[]args){
+//        try{
+//            MyThread thread=new MyThread();
+//            thread.start();
+//            Thread.sleep(1000);
+//            thread.interrupt();
+//            System.out.println("是否停止1？="+thread.interrupted());
+//            System.out.println("是否停止2？="+thread.isInterrupted());
+//        }catch(InterruptedException e){
+//            System.out.println("main catch");
+//            e.printStackTrace();
+//        }
+//        System.out.println("end!");
+//    }
+
+    //Thread.interrupted()具有清除中断的作用
+    public static void main(String[] args) {
+        Thread.currentThread().interrupt();
+        System.out.println("是否停止1？=" + Thread.interrupted());//True
+        System.out.println("是否停止2？=" + Thread.interrupted());//False
+        System.out.println("end!");
+    }
+}
+

0a1a92231eb31720fe8d9a6b704d9afa3ec57953
Jackchang <1114950132@qq.com>
2021-05-02 07:18:45 +0800

    Thread:communication:线程通信

---
+++ src/main/java/thread/communication/MyList.java
@@ -0,0 +1,15 @@
+package thread.communication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyList {
+    private List list=new ArrayList();
+    public void add(){
+        list.add("CHANGJS");
+    }
+    public int size(){
+        return list.size();
+    }
+}
+

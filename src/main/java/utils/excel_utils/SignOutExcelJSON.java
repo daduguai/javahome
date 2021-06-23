da1960fce8244ad03a0e14c86aac8155684aeb9c
Jackchang <1114950132@qq.com>
2021-06-23 23:19:10 +0800

    Thread:utils:excel utils

---
+++ src/main/java/utils/excel_utils/SignOutExcelJSON.java
@@ -0,0 +1,40 @@
+package utils.excel_utils;
+
+public class SignOutExcelJSON {
+    private String account;
+    private String username;
+    private String signdate;
+    private String finish;
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSigndate() {
+        return signdate;
+    }
+
+    public void setSigndate(String signdate) {
+        this.signdate = signdate;
+    }
+
+    public String getFinish() {
+        return finish;
+    }
+
+    public void setFinish(String finish) {
+        this.finish = finish;
+    }
+}

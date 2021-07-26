e366f5d398688179cdfdb5bbb08e42c305bddcf5
Jackchang <1114950132@qq.com>
2021-07-26 23:14:17 +0800

    leetcode:sequence:leetcode-4:两个正序数组查找中位数

---
+++ src/main/java/leetcode/LeetCode4_02.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * 两个正序数组的中位数*/
+public class LeetCode4_02 {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int mid1=nums1.length/2;
+        int mid2=nums2.length/2;
+        int k=(nums1.length+nums2.length)/2;
+        int odd=(nums1.length+nums2.length)%2;
+        while(k>0){
+            if(nums1[mid1]<nums2[mid2]){
+
+            }
+        }
+        return 0;
+    }
+}
+/**
+ * 采用类似于二分查找方法，时间复杂度O(log(m+n)),空间复杂度O(1)*/

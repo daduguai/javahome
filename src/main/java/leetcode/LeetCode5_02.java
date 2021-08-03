a78251338790cd89d25c684c1cd2fd560e272a8d
Jackchang <1114950132@qq.com>
2021-08-03 23:26:16 +0800

    leetcode:sequence:leetcode-5:最大回文子串

--- src/main/java/leetcode/LeetCode5_02.java
+++ src/main/java/leetcode/LeetCode5_02.java
@@ -6,14 +6,43 @@ import utils.Print;
 public class LeetCode5_02 {
     public static String longestPalindrome(String s){
         //假设字符串中不含字符$,使用$填充，将字符串变成奇数串
-        String res=null;
+        if(s.isEmpty()||s.length()==1){
+            return s;
+        }
+        String res="";
         String str="$";
         for(int i=0;i<s.length();i++){
             str=str+s.charAt(i)+"$";
         }
         Print.println(str);
 
+        int[] mncher=new int[str.length()];
+        mncher[0]=1;
+        int length=1;
+        int maxRight=0;//最右端
+        int maxLenth=0;//记录最大长度
+        int maxPos=0;
+        int pos=0;//最长点坐标
 
+        for(int i=0;i<str.length();i++){
+            if(i<maxRight){
+                mncher[i]=Math.min(mncher[2*pos-i],maxRight-i);
+            }else{
+                mncher[i]=1;
+            }
+            while(i-mncher[i]>=0&&i+mncher[i]<str.length()&&str.charAt(i+mncher[i])==str.charAt(i-mncher[i])){
+                mncher[i]++;
+            }
+            if(mncher[i]+i-1>maxRight){
+                maxRight=mncher[i]+i-1;
+                pos=i;
+            }
+            if(mncher[i]>=maxLenth){
+                maxLenth=mncher[i];
+                maxPos=pos;
+            }
+        }
+        res=s.substring(pos/2,2*maxLenth-1);
         return res;
     }
 

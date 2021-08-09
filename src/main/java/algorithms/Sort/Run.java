06067db336711e23395d2d188d7a11be8af0c409
Jackchang <1114950132@qq.com>
2021-08-09 22:23:17 +0800

    algorithom:sort:quickSort--快速排序

--- src/main/java/algorithms/Sort/Run.java
+++ src/main/java/algorithms/Sort/Run.java
@@ -4,7 +4,7 @@ import utils.Print;
 
 public class Run {
     public static void main(String[] args){
-        int[] data={6,5,4,3,2,1};
+        int[] data={2,2,2,2,2,2,2,1};
         Print.println("Before:");
         for(int i=0;i<data.length;i++){
             Print.println(data[i]);

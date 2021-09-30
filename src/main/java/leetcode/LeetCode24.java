0e497126e204c0775e6b025f6bbcc919c9bff4ce
Jackchang <1114950132@qq.com>
2021-10-01 00:40:39 +0800

    leetcode:sequence:leetcode-24:交换链表相邻节点

---
+++ src/main/java/leetcode/LeetCode24.java
@@ -0,0 +1,15 @@
+package leetcode;
+/**
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ *
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。*/
+public class LeetCode24 {
+    public static ListNode swapPairs(ListNode head) {
+        if(head==null||head.next==null){
+            return head;
+        }
+        ListNode hd=head.next;//假设不带头结点
+
+        return null;
+    }
+}
